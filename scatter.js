eaf81418a142f8bec2c5f581f2cf98fa37d4ad11
Adam Washington <a.washington@sheffield.ac.uk>
2016-10-04 16:23:53 +0100

    Remove lint

--- scatter.js
+++ scatter.js
@@ -1,12 +1,12 @@
 function total_scattering(value) {
-    var delta = value.sample-value.solvent
+    var delta = value.sample-value.solvent;
     return value.wavelength * value.wavelength * value.thickness * delta * delta * value.concentration * (1-value.concentration) * value.xsi;
-};
+}
 
 function update_label(f) {
     return function(d, i) {
         var style = "white";
-        if(isNaN(this.value) || this.value==="") {style="red"}
+        if(isNaN(this.value) || this.value==="") {style="red";}
         else {
             var new_value = +this.value;
             d3.select("#results")
@@ -14,12 +14,15 @@ function update_label(f) {
 
             update_values();
         }
-        d3.select(this).style("background-color",style)};};
+        d3.select(this).style("background-color",style);
+    };
+}
 
 function make_label(selector, f) {
     d3.select(selector)
         .on("input change",
-            update_label(f));};
+            update_label(f));
+}
 
 var def = {wavelength: +document.getElementById("wavelength").value,
            thickness: 1e7*(+document.getElementById("thickness").value),
@@ -34,28 +37,28 @@ d3.select("#results")
 
 make_label("#wavelength",
             function(new_value) {
-                return function(d) { d.wavelength = new_value; return d;}});
+                return function(d) { d.wavelength = new_value; return d;};});
 make_label("#thickness",
             function(new_value) {
-                return function(d) { d.thickness = new_value * 1e7; return d;}});
+                return function(d) { d.thickness = new_value * 1e7; return d;};});
 make_label("#sample",
             function(new_value) {
-                return function(d) { d.sample = new_value * 1e-6; return d;}});
+                return function(d) { d.sample = new_value * 1e-6; return d;};});
 make_label("#solvent",
             function(new_value) {
-                return function(d) { d.solvent = new_value * 1e-6; return d;}});
+                return function(d) { d.solvent = new_value * 1e-6; return d;};});
 make_label("#concentration",
             function(new_value) {
-                return function(d) { d.concentration = new_value * 1e-2; return d;}});
+                return function(d) { d.concentration = new_value * 1e-2; return d;};});
 make_label("#xsi",
             function(new_value) {
-                return function(d) { d.xsi = new_value * 10; return d;}});
+                return function(d) { d.xsi = new_value * 10; return d;};});
 make_label("#tune",
             function(new_value) {
-                return function(d) { d.tune = new_value; return d;}});
+                return function(d) { d.tune = new_value; return d;};});
 //Line Chart
 
-var margin = {top: 20, right: 20, bottom: 30, left: 50}
+var margin = {top: 20, right: 20, bottom: 30, left: 50};
 var width = 640 - margin.left - margin.right;
 var height = 480 - margin.top - margin.bottom;
 
@@ -78,9 +81,9 @@ var line = d3.line()
     .y(function(d) {return y(d[1]);});
 
 
-var graphx = []
+var graphx = [];
 for(i=0;i<10;i += 0.1) {
-    graphx.push(i)
+    graphx.push(i);
 }
 
 
@@ -98,21 +101,21 @@ function makePlot(selector,xaxis,xlabel,lineClass) {
         .attr("text-anchor", "middle")
         .attr("x", width/2)
         .attr("y",height-6)
-        .text(xlabel)
+        .text(xlabel);
     svg.append("text")
         .attr("class","y label")
         .attr("text-anchor", "middle")
         .attr("x",-height/2)
         .attr("y", 15)
         .attr("transform","rotate(-90)")
-        .text("Polarisation")
+        .text("Polarisation");
     svg.append("g")
-        .call(d3.axisLeft(y))
+        .call(d3.axisLeft(y));
     svg.append("path")
-        .data([graphx.map(function(i){return [i,1]})])
+        .data([graphx.map(function(i){return [i,1];})])
         .attr("class", "line " + lineClass)
         .attr("d", valueline(xaxis));
-    return svg
+    return svg;
 }
 
 var wsvg = makePlot("#wplot",wx,"Wavelength (Å)","wline");
@@ -122,7 +125,7 @@ function valueline(xaxis) {
     return d3.line()
             .x(function(d){return xaxis(d[0]);})
             .y(function(d){return y(d[1]);});
-};
+}
 
 /// Uopdate Functions
 
@@ -134,8 +137,8 @@ function update_values(){
         .select("#polarisation")
         .text(function(x) {return Math.exp(-total_scattering(x));});
 
-    var value = d3.select("#results").data()[0]
-    var wave = value.wavelength
+    var value = d3.select("#results").data()[0];
+    var wave = value.wavelength;
 
     d3.select("#results")
         .select("#spin_echo")
@@ -145,15 +148,15 @@ function update_values(){
         .data([graphx.map(function(d){
             value.wavelength = d;
             return [d, Math.exp(-total_scattering(value))];
-        })])
+        })]);
 
     d3.select(".sline")
         .data([graphx.map(function(d){
             value.wavelength = d;
             return [d*d*value.tune, Math.exp(-total_scattering(value))];
-        })])
+        })]);
 
-    value.wavelength = wave
+    value.wavelength = wave;
 
     d3.select(".wline")
         .transition()
@@ -174,4 +177,4 @@ function update_values(){
         .call(d3.axisBottom(sx));
 }
 
-update_values()
+update_values();
